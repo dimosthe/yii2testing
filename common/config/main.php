caa52926052d4b36c7eff370680a9bd665b3e232
George Dimosthenous <georgios.dimo@gmail.com>
2014-11-10 21:55:16 +0200

    configuration added for yii2-user module

--- common/config/main.php
+++ common/config/main.php
@@ -6,4 +6,14 @@ return [
             'class' => 'yii\caching\FileCache',
         ],
     ],
+    'modules' => [
+        'user' => [
+            'class' => 'dektrium\user\Module',
+            'enableUnconfirmedLogin' => true,
+            'confirmWithin' => 21600,
+            'cost' => 12,
+            'admins' => ['admin']
+        ],
+    ]
+
 ];
