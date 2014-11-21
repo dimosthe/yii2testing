d7d4d5b83fce4150b929faa25f2e2f08cb5254b7
dimosthe <georgios.dimo@gmail.com>
2014-11-21 17:45:11 +0200

    working on user-admin integration

--- common/config/main.php
+++ common/config/main.php
@@ -5,6 +5,12 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+         'authManager' => [
+            'class' => 'yii\rbac\PhpManager', // or use 'yii\rbac\DbManager'
+            'itemFile' => '@console/data/items.php',
+            'assignmentFile' => '@console/data/assignments.php',
+            'ruleFile' => '@console/data/rules.php'
+        ]
     ],
     'modules' => [
         'user' => [
@@ -14,6 +20,24 @@ return [
             'cost' => 12,
             'admins' => ['admin']
         ],
-    ]
+         'admin' => [
+            'class' => 'mdm\admin\Module',
+            'controllerMap' => [
+                 'assignment' => [
+                    'class' => 'mdm\admin\controllers\AssignmentController',
+                    'userClassName' => 'dektrium\user\models\User',
+                    'idField' => 'id', // id field of model User
+                ],
+            ],
+            'layout' => 'left-menu', // default null. other avaliable value 'right-menu' and 'top-menu'
+        
+        ]
+    ],
+    /*'as access' => [
+        'class' => 'mdm\admin\components\AccessControl',
+        'allowActions' => [
+            'admin/*', // add or remove allowed actions to this list
+        ]
+    ],*/
 
 ];
